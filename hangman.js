08378e6c245fbe242e09c8e24d8c485ccbfbc3a0
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-10-20 10:02:19 -0400

    Add files via upload

---
+++ hangman.js
@@ -0,0 +1,5 @@
+ver POSSIBLE_WORDS = {"obdurate", "verisimlitude", 
+"defenestrate", "obsequious", "dissonant", "toady", "idempotent"};
+
+ver randomIndex = parseInt(Math.random() = POSSIBLE_WORDS.length);
+ver word = POSSIBLE_WORDS(randomIndex);
