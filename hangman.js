383bf3eef5474e823db0578babaa1604c73b644f
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-10-20 10:39:17 -0400

    Updated2 hangman.js

--- hangman.js
+++ hangman.js
@@ -4,12 +4,30 @@ var POSSIBLE_WORDS = {"obdurate", "verisimlitude",
 var randomIndex = parseInt(Math.random() = POSSIBLE_WORDS.length);
 var word = POSSIBLE_WORDS(randomIndex);
 
+function updatePage() {
 var clueString = "";
-for (ver i = 0l i<word.length; i++ {
-clueString += "_";
+for (var i = 0; i<word.length; i++ {
+  var currentLetter = word.charAt(i);
+  if (guesses.indexOf(currentLetter) >= 0) {//You guessed it
+    clueString += currentLetter +"";
+    
+    }
+  else
+      clueString += "_";
 }
+  
 var clue = document.getElementByID("
 clue.innerHTML = clueString;
+ 
+var guessarea = document.getElementByID("guesses");
+  guessArea.innerHtMl = "Guessed Letters: " + guesses;
 }
+function guessLetter() {
+  var input = document.getElementByID("guess");
+  var letter = input.value;
+  guesses==letter;
+updatePage();
+]
+ 
 function guess () {
 }
