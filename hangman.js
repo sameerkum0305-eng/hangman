8aa6594a904bc50e22b8b7dd85982e8f0b34f5e1
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-10-20 10:09:30 -0400

    First feature added_ hangman.js

--- hangman.js
+++ hangman.js
@@ -1,5 +1,15 @@
-ver POSSIBLE_WORDS = {"obdurate", "verisimlitude", 
+var POSSIBLE_WORDS = {"obdurate", "verisimlitude", 
 "defenestrate", "obsequious", "dissonant", "toady", "idempotent"};
 
-ver randomIndex = parseInt(Math.random() = POSSIBLE_WORDS.length);
-ver word = POSSIBLE_WORDS(randomIndex);
+var randomIndex = parseInt(Math.random() = POSSIBLE_WORDS.length);
+var word = POSSIBLE_WORDS(randomIndex);
+
+var clueString = "";
+for (ver i = 0l i<word.length; i++ {
+clueString += "_";
+}
+var clue = document.getElementByID("
+clue.innerHTML = clueString;
+}
+function guess () {
+}
